b4288fbb746092f9afb0fd15a81521595e887377
nuekleer <matthewrchristensen@gmail.com>
2020-06-05 00:15:33 -0500

    base server static html up and running

---
+++ src/server/server.js
@@ -0,0 +1,27 @@
+var path = require('path')
+const express = require('express')
+
+/* Dependencies */
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express()
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+app.use(cors());
+
+app.use(express.static('dist'))
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(3000, function () {
+    console.log('Listening on port 3000!')
+})
